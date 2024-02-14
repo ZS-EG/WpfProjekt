c28d56dcf48e02624d40a02ea7bef6498975b00c
ZS-EG <grannygrandpacool@gmail.com>
2024-02-14 08:32:12 +0100

    z dzwiekiem

--- WpfProjekt/MainWindow.xaml.cs
+++ WpfProjekt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,7 @@ namespace WpfProjekt
         public int punktyGracz = 0;
         public int punktyAuto = 0;
         public int Nr { get; set; }
+        public int Losowana { get; set; }
         public List<BitmapImage> Images {  get; set; }
         public MainWindow()
         {
@@ -30,52 +32,56 @@ namespace WpfProjekt
         {
             Random random = new Random();
             int losowana = random.Next(0, 3);
-            imgAuto.Source = Images[losowana];
+            Losowana = losowana;
+            imgAuto.Source = Images[Losowana];
             //MessageBox.Show(losowana.ToString());
+            sprawdzWynik();
+            wynik.Text = punktyGracz + " : " + punktyAuto;
+            //SystemSounds.Hand.Play();
+        }
 
-            
-            if(losowana == Nr)
+        private void sprawdzWynik()
+        {
+            if (Losowana == Nr)
             {
                 historia.Text = "Remis.";
             }
 
-
-            if (losowana == 0 && Nr == 1)
+            //kamien < papier < nozyczki < kamien
+            if (Losowana == 0 && Nr == 1)
             {
                 punktyGracz++;
                 historia.Text = "Wygrywa Gracz, Papier wygrywa z kamieniem.";
             }
-            else if(losowana == 1 && Nr == 0)
+            else if (Losowana == 1 && Nr == 0)
             {
                 punktyAuto++;
                 historia.Text = "Wygrywa Komputer, Papier wygrywa z kamieniem.";
             }
-            
-            
-            if (losowana == 0 && Nr == 2)
+
+
+            if (Losowana == 0 && Nr == 2)
             {
                 punktyAuto++;
                 historia.Text = "Wygrywa Auto, Kamien wygrywa z nozyczkami.";
             }
-            else if (losowana == 2 && Nr == 0)
+            else if (Losowana == 2 && Nr == 0)
             {
                 punktyGracz++;
                 historia.Text = "Wygrywa Gracz, Kamien wygrywa z nozyczkami.";
             }
 
 
-            if (losowana == 1 && Nr == 2)
+            if (Losowana == 1 && Nr == 2)
             {
                 punktyGracz++;
                 historia.Text = "Wygrywa Gracz, Nozyczki wygrywa z papierem.";
             }
-            else if (losowana == 2 && Nr == 1)
+            else if (Losowana == 2 && Nr == 1)
             {
                 punktyAuto++;
                 historia.Text = "Wygrywa Auto, Nozyczki wygrywa z papierem.";
             }
-
-            wynik.Text = punktyGracz + " : " + punktyAuto;
         }
 
         private void Button_K(object sender, RoutedEventArgs e)
@@ -83,6 +89,7 @@ namespace WpfProjekt
             //Kamien
             Nr = 0;
             imgGracz.Source = Images[Nr];
+            SystemSounds.Asterisk.Play();
         }
 
         private void Button_P(object sender, RoutedEventArgs e)
@@ -90,6 +97,7 @@ namespace WpfProjekt
             //Papier
             Nr = 1;
             imgGracz.Source = Images[Nr];
+            SystemSounds.Beep.Play();
         }
 
         private void Button_N(object sender, RoutedEventArgs e)
@@ -97,6 +105,7 @@ namespace WpfProjekt
             //Nozyce
             Nr = 2;
             imgGracz.Source = Images[Nr];
+            SystemSounds.Exclamation.Play();
         }
         private void przygotujGre()
         {
@@ -105,6 +114,20 @@ namespace WpfProjekt
             Images.Add(new BitmapImage(new Uri("img/papier.jpg", UriKind.Relative)));
             Images.Add(new BitmapImage(new Uri("img/nozyczki.jpg", UriKind.Relative)));
         }
+
+
+        private void MenuItem_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_ResetWynik(object sender, RoutedEventArgs e)
+        {
+            punktyAuto = 0;
+            punktyGracz = 0;
+            wynik.Text = punktyGracz + " : " + punktyAuto;
+            SystemSounds.Question.Play();
+        }
     }
 }
 
@@ -112,6 +135,4 @@ namespace WpfProjekt
 0 - kamien
 1 - papier
 2 - nozyczki
-
-kamien < papier < nozyczki < kamien
  */
